1aaf3323373cbe3053c463be008b7a0c0cd1fe32
p15mago <p15mago@ionio.gr>
2018-02-19 00:15:28 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -92,10 +92,11 @@ function start() {
 		.attr("r", 0)
 		.style("fill", function(d) { return fill(d.party); })
 		.on("mouseover", mouseover)
-		.on("mouseout", mouseout);
+		.on("mouseout", mouseout)
 		// Alternative title based 'tooltips'
 		// node.append("title")
 		//	.text(function(d) { return d.donor; });
+		.on("click",search);
 
 		force.gravity(0)
 			.friction(0.75)
@@ -307,6 +308,7 @@ function display(data) {
 	return start();
 }
 function search(d) {
+	var donor = d.donor;
 	window.open("https://www.google.com/search?q=" + donor);
 }
 		    
